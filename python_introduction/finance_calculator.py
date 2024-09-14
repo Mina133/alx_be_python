370d40053414bbc8d8dae4e8c42df03cf73e0697
Mina Markos <minalxr123@gmail.com>
2024-09-15 01:54:33 +0300

    edit0 advance poject is done

--- python_introduction/finance_calculator.py
+++ python_introduction/finance_calculator.py
@@ -1,7 +1,7 @@
 monthely_income = int(input("Enter your monthly income: "))
 monthely_expenses= int(input("Enter your monthly expenses: "))
-monthely_saving = monthely_income - monthely_expenses
-project_saving = int(monthely_saving*12+(monthely_saving*12*0.05))
+monthely_savings = monthely_income - monthely_expenses
+project_saving = int(monthely_savings*12+(monthely_savings*12*0.05))
 
-print("Your monthly saving are $", monthely_saving)
+print("Your monthly saving are $", monthely_savings)
 print("Projection savings after one year, with interest, is: $", project_saving)
