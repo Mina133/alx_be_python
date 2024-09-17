6db0841a1068ec49420776bd3f925fdcf39cea8d
Mina Markos <minalxr123@gmail.com>
2024-09-17 23:23:12 +0300

    the calculation is done *

--- control-flow/match_case_calculator.py
+++ control-flow/match_case_calculator.py
@@ -10,6 +10,7 @@ elif operation == "-":
     print("The result is", result)
 elif operation == "*":
     result = num1 * num2
+    print("The result is", result)
 elif operation == "/":
     if num2 == 0:
         print("Cannot divide by zero.")
