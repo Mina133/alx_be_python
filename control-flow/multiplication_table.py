c74d5c6030476b87f362812b5fc92eb223045154
Mina Markos <minalxr123@gmail.com>
2024-09-18 00:02:21 +0300

    update range parameters

--- control-flow/multiplication_table.py
+++ control-flow/multiplication_table.py
@@ -1,4 +1,4 @@
 number = int(input("Enter a number to see its multiplication table:"))
 
-for i in range(10):
-    print(number, "x", i+1, "=", number*(i+1))
+for i in range(1,11):
+    print(number, "x", i, "=", number*i)
