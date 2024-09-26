c695c8d1813873e1fde3e340d3f241050acdf10e
Mina Markos <minalxr123@gmail.com>
2024-09-26 09:22:08 +0300

    Arthimitic operation (delete error)

--- fns_and_dsa/arithmetic_operations.py
+++ fns_and_dsa/arithmetic_operations.py
@@ -12,13 +12,3 @@ def perform_operation ( num1,  num2,  operation):
     else:
         return 'Invalid operation'
     
-
-def main():
-    print("Arithmetic Operations")
-    num1 = float(input("Enter the first number: "))
-    num2 = float(input("Enter the second number: "))
-    operation = input("Enter the operation (add, subtract, multiply, divide): ")
-    print("Result: ", perform_operation(num1, num2, operation)) 
-
-if __name__ == "__main__":
-    main()
