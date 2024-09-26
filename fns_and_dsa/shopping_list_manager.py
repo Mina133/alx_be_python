cb2aa242aa1c96bea626c9999e7947962bdfead6
Mina Markos <minalxr123@gmail.com>
2024-09-26 09:43:14 +0300

    Market zy L 3asal

---
+++ fns_and_dsa/shopping_list_manager.py
@@ -0,0 +1,32 @@
+def display_menu():
+    print("Shopping List Manager")
+    print("1. Add Item")
+    print("2. Remove Item")
+    print("3. View List")
+    print("4. Exit")
+
+def main():
+    shopping_list = []
+    while True:
+        display_menu()
+        choice = input("Enter your choice: ")
+
+        if choice == '1':
+            newItem = input("Enter the item: ")
+            shopping_list.append(newItem)
+            pass
+        elif choice == '2':
+            itemToRemove = input("Enter the item to remove: ")
+            shopping_list.remove(itemToRemove)
+            pass
+        elif choice == '3':
+            print("Shopping List:", shopping_list)
+            pass
+        elif choice == '4':
+            print("Goodbye!")
+            break
+        else:
+            print("Invalid choice. Please try again.")
+
+if __name__ == "__main__":
+    main()
