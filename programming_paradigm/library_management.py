72f010a7c0da3dd6a4ee3265882b616cccea2af1
Mina Markos <minalxr123@gmail.com>
2024-10-10 17:24:41 +0300

    oop

---
+++ programming_paradigm/library_management.py
@@ -0,0 +1,35 @@
+class Book:
+    def __init__(self, title, author):
+        self.title = title
+        self.author = author
+        self.is_available = True
+
+   
+class Library:
+    def __init__(self):
+        self.books = []
+
+    def add_book(self, book):
+        self.books.append(book)
+
+    def check_out_book(self, title):
+        for book in self.books:
+            if book.title == title and book.is_available:
+                book.is_available = False
+                return True
+        return False
+    
+    def return_book(self, title):
+        for book in self.books:
+            if book.title == title and not book.is_available:
+                book.is_available = True
+                return True
+        return False
+    
+    def list_available_books(self):
+        available_books =[]
+        for book in self.books:
+            if book.is_available:
+                available_books.append(book.title)
+        return available_books
+ 
