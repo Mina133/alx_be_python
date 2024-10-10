3e9ca813bc362e9d32aaad239aa4c2ad4d678ae0
Mina Markos <minalxr123@gmail.com>
2024-10-10 17:10:00 +0300

    spilling change

--- programming_paradigm/test_simple_calculator.py
+++ programming_paradigm/test_simple_calculator.py
@@ -1,27 +1,27 @@
 import unittest
 from simple_calculator import SimpleCalculator
 
-class TestSimpleCalculator(unittest.TestCase):
+class TestSimplecalc(unittest.TestCase):
     def setUp(self):
-        self.calculator = SimpleCalculator()
+        self.calc = SimpleCalculator()
 
-    def test_add(self):
-        self.assertEqual(self.calculator.add(1, 1), 2)
-        self.assertEqual(self.calculator.add(1, -1), 0)
-        self.assertEqual(self.calculator.add(-1, -1), -2)
+    def test_addition(self):
+        self.assertEqual(self.calc.add(1, 1), 2)
+        self.assertEqual(self.calc.add(1, -1), 0)
+        self.assertEqual(self.calc.add(-1, -1), -2)
 
-    def test_subtract(self):
-        self.assertEqual(self.calculator.subtract(1, 1), 0)
-        self.assertEqual(self.calculator.subtract(1, -1), 2)
-        self.assertEqual(self.calculator.subtract(-1, -1), 0)
+    def test_subtraction(self):
+        self.assertEqual(self.calc.subtract(1, 1), 0)
+        self.assertEqual(self.calc.subtract(1, -1), 2)
+        self.assertEqual(self.calc.subtract(-1, -1), 0)
 
     def test_multiply(self):
-        self.assertEqual(self.calculator.multiply(1, 1), 1)
-        self.assertEqual(self.calculator.multiply(1, -1), -1)
-        self.assertEqual(self.calculator.multiply(-1, -1), 1)
+        self.assertEqual(self.calc.multiply(1, 1), 1)
+        self.assertEqual(self.calc.multiply(1, -1), -1)
+        self.assertEqual(self.calc.multiply(-1, -1), 1)
 
     def test_divide(self):
-        self.assertEqual(self.calculator.divide(1, 1), 1)
-        self.assertEqual(self.calculator.divide(1, -1), -1)
-        self.assertEqual(self.calculator.divide(-1, -1), 1)
-        self.assertIsNone(self.calculator.divide(1, 0))
+        self.assertEqual(self.calc.divide(1, 1), 1)
+        self.assertEqual(self.calc.divide(1, -1), -1)
+        self.assertEqual(self.calc.divide(-1, -1), 1)
+        self.assertIsNone(self.calc.divide(1, 0))
